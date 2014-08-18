350a76cb0ea836676db5784e1bb85eaa9aa15807
zz张智 <zhangzhi@ctrip.con>
2014-08-18 10:05:36 +0800

    修改了build.xml

---
+++ skyseas_plugins/src/plugins/openmessage/src/main/com/skyseas/openfireplugins/openmessage/OpenMessagePlugin.java
@@ -0,0 +1,20 @@
+package com.skyseas.openfireplugins.openmessage;
+
+import java.io.File;
+
+import org.jivesoftware.openfire.container.Plugin;
+import org.jivesoftware.openfire.container.PluginManager;
+
+public class OpenMessagePlugin implements Plugin {
+
+	@Override
+	public void destroyPlugin() {
+		
+	}
+
+	@Override
+	public void initializePlugin(PluginManager arg0, File arg1) {
+		
+	}
+
+}
